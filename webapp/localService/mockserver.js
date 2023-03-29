63cf38b88bb80537e9b700fdca62199e04a780a0
Janet Gloria <janet.gloria@commsult.id>
2023-03-29 16:12:19 +0200

    running locally and broken integration Qunit test

--- webapp/localService/mockserver.js
+++ webapp/localService/mockserver.js
@@ -1,30 +1,30 @@
-sap.ui.define(
-  ["sap/ui/core/util/MockServer", "sap/base/util/UriParameters"],
-  function (MockServer, UriParameters) {
-    "use strict";
+// sap.ui.define(
+//   ["sap/ui/core/util/MockServer", "sap/base/util/UriParameters"],
+//   function (MockServer, UriParameters) {
+//     "use strict";
 
-    return {
-      init: function () {
-        // create
-        var oMockServer = new MockServer({
-          rootUri: "https://services.odata.org/V2/Northwind/Northwind.svc/",
-        });
+//     return {
+//       init: function () {
+//         // create
+//         var oMockServer = new MockServer({
+//           rootUri: "https://services.odata.org/V2/Northwind/Northwind.svc/",
+//         });
 
-        var oUriParameters = new UriParameters(window.location.href);
+//         var oUriParameters = new UriParameters(window.location.href);
 
-        // configure mock server with a delay
-        MockServer.config({
-          autoRespond: true,
-          autoRespondAfter: oUriParameters.get("serverDelay") || 500,
-        });
+//         // configure mock server with a delay
+//         MockServer.config({
+//           autoRespond: true,
+//           autoRespondAfter: oUriParameters.get("serverDelay") || 500,
+//         });
 
-        // simulate
-        var sPath = "../localService";
-        oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
+//         // simulate
+//         var sPath = "../localService";
+//         oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
 
-        // start
-        oMockServer.start();
-      },
-    };
-  }
-);
+//         // start
+//         oMockServer.start();
+//       },
+//     };
+//   }
+// );
